774cd52feecdeeb47653aa5396b1bbb06fe38012
Tie Ma <TIEMA@CMAIL.CARLETON.CA>
2025-02-12 16:41:46 -0500

    Update readme.m

--- The Matlab code/Matlab/readme.m
+++ The Matlab code/Matlab/readme.m
@@ -1,9 +1,9 @@
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%readme
+%Readme update 2025/01/20
 %now, all the data clearning and processing part are able to funcation
-%automatrically in teh main_program.
-
+%automatrically in the main_program.m.
+%note: You cannot run this program as there is no data
 
 
 
@@ -34,33 +34,4 @@
 
   
 
-
-%The index for the data processing code 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Table of Contents
-% 1. Setup and Data Loading (line 17)
-% 2. Data Cleaning and Filtering (line 52)
-% 3. Create Timetable for Each Country (line 68)
-% 4. Log Transformation of GDP Data (line 84)
-% 5. HP Filter to Detrend GDP Data (line 104)
-% 6. Quadratic Detrending of GDP Data (line 124)
-% 7. HP Filter for Inflation Data (line 148)
-% 8. Calculate Nominal GDP Growth Rate (line 168)
-% 9. Calculate Average Inflation Rates (line 199)
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-
-%The index for the two stage regression analysis
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Table of Contents
-% 1. Initialize and Load Data (line 14)
-% 2. Collect Data for Stage One Regression (line 37)
-% 3. Prepare Data for Stage One Regression (line 48)
-% 4. Perform and Store Stage One Regression (line 74)
-% 5. Perform and Store Stage Two Regression (line 142)
-% 6. Plot Persistence Value vs. Average Inflation(AI) (line 192)
-% 7. Plot Persistence Value vs. AI, AI^2 (line 235)
-% 8. Plot Impact Value vs. Average Inflation (line 280)
-% 9. Save Regression Results to Excel and CSV (line 321) 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
